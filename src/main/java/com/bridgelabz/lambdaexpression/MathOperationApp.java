14974fce25f9f8d6d3c1c02ae0f9e927f2e1f294
Balaji2624 <balajisapkal3062001@gmail.com>
2024-10-04 14:51:45 +0530

    Added: create a number play list and iterate and print each element use forEach to demonstrate iteration.

--- src/main/java/com/bridgelabz/lambdaexpression/MathOperationApp.java
+++ src/main/java/com/bridgelabz/lambdaexpression/MathOperationApp.java
@@ -1,23 +1,73 @@
 package com.bridgelabz.lambdaexpression;
 
-interface IMathFunction {
-    int calculate(int a, int b);
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
 
-    static void printResult(int a, int b, String functionName, IMathFunction mathFunction) {
-        System.out.println(functionName + " of " + a + " and " + b + " is: " + mathFunction.calculate(a, b));
+class NumberPlayList {
+
+    // List to store numbers
+    private List<Integer> numberList = new ArrayList<>();
+
+    // Method to add numbers to the list
+    public void addNumber(Integer number) {
+        numberList.add(number);
+    }
+
+    // Method to iterate through the list using forEach
+    public void iterateUsingClass() {
+        // Using an external class implementation of Consumer
+        class MyConsumer implements Consumer<Integer> {
+            public void accept(Integer number) {
+                System.out.println("Class-based iteration: " + number);
+            }
+        }
+
+        // Iterate and print each element using an external class
+        numberList.forEach(new MyConsumer());
+    }
+
+    // Method to iterate using an Anonymous class
+    public void iterateUsingAnonymousClass() {
+        // Using an anonymous class to implement Consumer
+        numberList.forEach(new Consumer<Integer>() {
+            @Override
+            public void accept(Integer number) {
+                System.out.println("Anonymous class iteration: " + number);
+            }
+        });
+    }
+    public void iterateUsingLambda() {
+        // Using lambda function for iteration
+        numberList.forEach(number -> System.out.println("Lambda iteration: " + number));
     }
 }
+
+
 
 public class MathOperationApp {
     public static void main(String[] args) {
-        IMathFunction add = (x,y) -> x+y;
-        IMathFunction substract = (x,y) -> x-y;
-        IMathFunction multiply = (x,y) -> x*y;
-        IMathFunction divide = (x,y) -> x/y;
+        NumberPlayList numberPlayList = new NumberPlayList();
 
-        IMathFunction.printResult(5,4,"Addition",add);
-        IMathFunction.printResult(5,4,"Substraction",substract);
-        IMathFunction.printResult(5,4,"Multiply",multiply);
-        IMathFunction.printResult(5,4,"Divide",divide);
+        // Add numbers to the list
+        numberPlayList.addNumber(10);
+        numberPlayList.addNumber(20);
+        numberPlayList.addNumber(30);
+        numberPlayList.addNumber(40);
+
+        // Iterating using class
+        System.out.println("Iteration using Class:");
+        numberPlayList.iterateUsingClass();
+
+        // Iterating using anonymous class
+        System.out.println("\nIteration using Anonymous Class:");
+        numberPlayList.iterateUsingAnonymousClass();
+
+        // Iterating using lambda expression
+        System.out.println("\nIteration using Lambda:");
+        numberPlayList.iterateUsingLambda();
+
     }
 }
+
+
